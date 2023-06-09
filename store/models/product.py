1ede2cd7070a201b57bed94d5bfa32a17a420e3d
waqasbcs <waqasaliy2945@gmail.com>
2023-06-09 16:33:12 +0500

    first commit

---
+++ store/models/product.py
@@ -0,0 +1,11 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from .category import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description = RichTextField()
+    image = models.ImageField(upload_to='upload/products/')
