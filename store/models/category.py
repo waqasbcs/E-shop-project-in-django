198c36aeb0d749f95302fad93f306f8511c8292b
waqasbcs <waqasaliy2945@gmail.com>
2023-06-13 13:10:13 +0500

    templates updated

--- store/models/category.py
+++ store/models/category.py
@@ -4,6 +4,11 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     
     
+    @staticmethod
+    def get_all_categories():
+       return Category.objects.all()
+        
+    
     def __str__(self):
         return self.name
     
--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from . models.product import Product
+from . models.category import Category
+
 
 # Create your views here.
 def index(request):
-    products= Product.get_all_products();
-    return render(request, 'store/index.html',{'products': products})
+    products = Product.get_all_products();
+    categories = Category.get_all_categories();
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'store/index.html',data)
