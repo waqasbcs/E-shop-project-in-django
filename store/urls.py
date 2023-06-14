541fcc276c4c38a9f27df4d1ec705e0951be39b7
waqasbcs <waqasaliy2945@gmail.com>
2023-06-14 23:18:50 +0500

    signup page created

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, signup
 
 
 
 urlpatterns = [
-    path('',index,name='index')
+    path('', index,name='index'),
+    path('signup/' , signup,name='signup'),
 ]
--- store/views.py
+++ store/views.py
@@ -5,9 +5,18 @@ from . models.category import Category
 
 # Create your views here.
 def index(request):
-    products = Product.get_all_products();
-    categories = Category.get_all_categories();
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+     products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+       products = Product.get_all_products();
     data = {}
     data['products'] = products
     data['categories'] = categories
     return render(request, 'store/index.html',data)
+ 
+# signup form
+def signup(request):
+   return render(request, 'store/signup.html')
